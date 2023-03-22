9bf5117cd5a5924d06b26f2bcecd681387d35ed5
limeunbeen <mossi3627@naver.com>
2023-03-22 18:04:38 +0900

    Model (My Page) 추가

---
+++ deeplearning/src/main/java/com/javalec/base/model/Join_PostUploadUserWishModel.java
@@ -0,0 +1,52 @@
+package com.javalec.base.model;
+
+import java.sql.Date;
+
+public class Join_PostUploadUserWishModel {
+	int poHeart;
+	String poTitle;
+	String poPrice;
+	String poImage01;
+	int poViews;
+	int poState;
+
+
+	public int getPoHeart() {
+		return poHeart;
+	}
+	public void setPoHeart(int poHeart) {
+		this.poHeart = poHeart;
+	}
+	public String getPoTitle() {
+		return poTitle;
+	}
+	public void setPoTitle(String poTitle) {
+		this.poTitle = poTitle;
+	}
+	public String getPoPrice() {
+		return poPrice;
+	}
+	public void setPoPrice(String poPrice) {
+		this.poPrice = poPrice;
+	}
+	public String getPoImage01() {
+		return poImage01;
+	}
+	public void setPoImage01(String poImage01) {
+		this.poImage01 = poImage01;
+	}
+	public int getPoViews() {
+		return poViews;
+	}
+	public void setPoViews(int poViews) {
+		this.poViews = poViews;
+	}
+	public int getPoState() {
+		return poState;
+	}
+	public void setPoState(int poState) {
+		this.poState = poState;
+	}
+	
+
+}
