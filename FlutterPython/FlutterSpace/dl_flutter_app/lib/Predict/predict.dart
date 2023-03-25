c1f48705788c4a19d6936b32024a81099be467f7
MirrorSpring <dbtmd619@naver.com>
2023-03-25 14:29:03 +0900

    예측 메소드 개선

--- FlutterPython/FlutterSpace/dl_flutter_app/lib/Predict/predict.dart
+++ FlutterPython/FlutterSpace/dl_flutter_app/lib/Predict/predict.dart
@@ -1,15 +1,23 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class PredictImage {
   
     //이미지 올리기
-  Future<void> upload(image) async {
+  Future<String> upload(image) async {
     if (image != null) {
       var url = Uri.parse("http://localhost:5000/upload");
       var request = http.MultipartRequest("POST", url);
       request.files
           .add(await http.MultipartFile.fromPath('image', image!.path));
-      await request.send();
+      var response=await request.send();
+      var dataConvertedJSON = json.decode(await response.stream.bytesToString());
+      String result = dataConvertedJSON['result'];
+      return result;
+    }
+    else{
+      return 'error';
     }
   }
 }
