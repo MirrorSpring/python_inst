06c027fcc0fa924156d27ae7bdb44b2cff20b817
limeunbeen <mossi3627@naver.com>
2023-03-26 01:13:54 +0900

    UserInfoModel(회원정보) 추가

---
+++ deeplearning/src/main/java/com/javalec/base/model/UserInfoModel.java
@@ -0,0 +1,36 @@
+package com.javalec.base.model;
+
+public class UserInfoModel {
+	String userId;
+	String userName;
+	String userPw;
+	String userAddress;
+	
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+	public String getUserAddress() {
+		return userAddress;
+	}
+	public void setUserAddress(String userAddress) {
+		this.userAddress = userAddress;
+	}
+	
+	
+}
