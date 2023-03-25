0d68cf4b559d3640e61bf718f42c4f44ac8bf52e
limeunbeen <mossi3627@naver.com>
2023-03-26 01:05:02 +0900

    UserModel의 회원 정보 가져오고 수정하기 추가

--- FlutterPython/FlutterSpace/dl_flutter_app/lib/Model/User/user_mypage.dart
+++ FlutterPython/FlutterSpace/dl_flutter_app/lib/Model/User/user_mypage.dart
@@ -1,8 +1,28 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class UserModel {
+  // 회원 탈퇴
   Future deleteUser(String userId) async {
     var url = Uri.parse('http://localhost:8080/deleteUser/$userId');
+    var response = await http.get(url);
+  }
+
+  // 회원 정보 가져오기
+  Future<Map> userInfo(String userId) async {
+    var url = Uri.parse('http://localhost:8080/userInfo?userId=$userId');
+    // var url = Uri.parse('http://localhost:8080/user/all');
+    var respnse = await http.get(url);
+    var userInfo = json.decode(utf8.decode(respnse.bodyBytes));
+    return userInfo[0];
+  }
+
+  // 회원 정보 수정
+  Future updateUser(
+      String userId, String userPw, String userName, String userAddress) async {
+    var url = Uri.parse(
+        'http://localhost:8080/updateUser/$userId/$userPw/$userName/$userAddress');
     var response = await http.get(url);
   }
 }
